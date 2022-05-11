51fd70be9362110f399972bc9508c9e356699ba0
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-05-12 00:02:34 +0800

    Add files via upload

---
+++ Project15/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int i;
+int main()
+{
+    i--;
+    if (i > sizeof(i))
+    {
+        printf(">\n");
+    }
+    else
+    {
+        printf("<\n");
+    }
+    return 0;
+}
