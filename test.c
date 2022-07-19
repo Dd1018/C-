ec5b7b123d52b4dcd9924bedebead17053614e9f
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-07-19 21:48:24 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,27 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-//#include<stddef.h>
-//struct S
-//{
-//	char a;
-//	int b;
-//	int c;
-//};
-//#define OFFSETOF(type,name) (int)&(((type*)0)->name)
-//
-//int main()
-//{
-//	printf("%d ", OFFSETOF(struct S, a));
-//	printf("%d ", OFFSETOF(struct S, b));
-//	printf("%d ", OFFSETOF(struct S, c));
-//	return 0;
-//}
-#define SWAP(i) ((0xaaaaaaaa&i)>>1)|((0x55555555&i)<<1)
+#include"contact.h"
+void menu()
+{
+	printf("**************************************\n");
+	printf("**     1. add         2. del        **\n");
+	printf("**     3. search      4.modify      **\n");
+	printf("**     5. show        6.sort        **\n");
+	printf("**     0. exit                      **\n");
+	printf("**************************************\n");
+}
 int main()
 {
-	int a = 0;
-	scanf("%d", &a);
-	printf("%d",SWAP(a));
-	return 0;
 
+	int input;
+	struct Con infor;
+	Itni(&infor);
+	do
+	{
+		menu();
+		printf("ÇëÑ¡Ôñ>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			Add(&infor);
+			break;
+		case 2:
+			Del(&infor);
+			break;
+		case 3:
+			Search(&infor);
+			break;
+		case 4:
+			Modify(&infor);
+			break;
+		case 5:
+			Show(&infor);
+			break;
+		case 6:
+			Sort(&infor);
+			break;
+		case 0:
+			Save(&infor);
+			Destory(&infor);
+			printf("ÍË³ö\n");
+			break;
+		}
+	} while (input);
+	return 0;
 }
