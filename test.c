60e49edbb60653cbca88fdddaf23c94fedf9f681
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-07-24 21:50:25 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,37 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
-int cmp(int* e1, int* e2)
+fun(char *arr,int left, int right)
 {
-    return *e2 - *e1;
-}
-int findMaxConsecutiveOnes(int* nums, int numsSize) {
-    int i = 0;
-    int count = 0;
-    int j = 0;
-    int* p = (int*)malloc(numsSize * sizeof(int));
-    for (i = 0; i < numsSize; i++)
+    char tmp;
+    while (left < right)
     {
-        if (nums[i] == 1)
-        {
-            count++;
-        }
-        else
-        {
-            p[j++] = count;
-            count = 0;
-        }
+        tmp = arr[left];
+        arr[left]= arr[right];
+        arr[right] = tmp;
+       left++;
+        right--;
     }
-    p[j++] = count;
-    qsort(p, j, sizeof(int), cmp);
-    int z = p[0];
-    return z;
 }
 int main()
 {
-    int i = 1;
-    sizeof(i++);
-    printf("%d\n", i);
+    char arr[10000];
+    int i = 0;
+    int j = 0;
+    gets(arr);
+    fun(arr, 0, strlen(arr) - 1);
+        while (1)
+        {
+            while (arr[i] == ' '||arr[i]=='\0')
+            {
+                i--;
+                fun(arr,j, i);
+                j = i + 2;
+                i++;
+                break;
+            }
+            if (arr[i] != '\0')
+                i++;
+            else
+                break;
+        }
+    printf("%s", arr);
     return 0;
 }
