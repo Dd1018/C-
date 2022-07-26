4e7d16cc2df1db135d034b84b7bb9a473dc65367
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-07-26 20:24:43 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,55 +1,144 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-//
-//int main()
+#include<stdio.h>
+#include<stdlib.h>
+//void quick_qsort(int* arr, int left, int right)
 //{
-//    double n = 0.00;
-//    int m = 0;
-//
-//    while (~scanf("%lf %d", &n, &m))
-//    {
-//        double sum = 0.00;
-//        for (int i = 0; i < m; i++)
-//        {
-//            sum += n;
-//            n = sqrt(n);
-//        }
-//        printf("%.2lf\n", sum);
-//    }
-//
-//    return 0;
-//}
-//int main()
-//{
-//	char c = 'A';
-//	if ('0' <= c <= '9') printf("YES");
-//	else printf("NO");
-//	return 0;
-//}
-//#include<stdio.h>
-//int main()
-//{
-//	int n = 1001;
-//	int ans = 0;
-//	for (int i = 1; i <= n; ++i)
+//	if (left >= right)
+//		return;
+//	int i = left-1;
+//	int j = right+1;
+//	int x = arr[left];
+//	while (i < j)
 //	{
-//		ans ^= i % 3;
+//		do
+//		{
+//			i++;
+//		} while (arr[i] < x);
+//		do
+//		{
+//			j--;
+//		} while (arr[j] > x);
+//		if (i < j)
+//		{
+//			int tmp = arr[i];
+//			arr[i] = arr[j];
+//			arr[j] = tmp;
+//		}
 //	}
-//	printf("%d", ans);
+//	quick_qsort(arr, left, j);
+//	quick_qsort(arr, j + 1, right);
+//}
+//int main()
+//{
+//	int arr[50];
+//	int n;
+//	scanf("%d", &n);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	quick_qsort(arr, 0, n - 1);
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+////}
+//void merge_sort(int* arr, int l, int r)
+//{
+//	if (l >= r)
+//		return;
+//	int mid = (l + r) / 2;
+//	merge_sort(arr, l, mid );
+//	merge_sort(arr, mid+1, r);
+//	int tmp[100];
+//	int i = l;
+//	int j = mid + 1;
+//	int k = 0;
+//	while (i <= mid&&j<=r)
+//	{
+//		if (arr[i] < arr[j])
+//			tmp[k++] = arr[i++];
+//		else
+//			tmp[k++] = arr[j++];
+//	}
+//	while (i <= mid)
+//		tmp[k++] = arr[i++];
+//	while (j <= r)
+//		tmp[k++] = arr[j++];
+//
+//	for (i = l,j=0; i <=r; i++,j++)
+//	{
+//		arr[i] = tmp[j];
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int arr[100];
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	merge_sort(arr, 0, n-1);
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
 //	return 0;
 //}
-int fun(unsigned int x)
-{
-	int n = 0;
-	while (x + 1)
-	{
-		n++;
-		x = x | (x + 1);
-	} return n;
-}
 int main()
 {
-	fun(2014);
-	return 0;
+    int n;
+    int m;
+    scanf("%d%d", &n, &m);
+    int i = 0;
+    int arr[100000];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int k = 0;
+
+    while (m--)
+    {
+        scanf("%d", &k);
+        int l = 0;
+        int r = n - 1;
+        while (l < r)
+        {
+            int mid = (l + r + 1) / 2;
+            if (k >= arr[mid])
+            {
+                l = mid + 1;
+            }
+            else
+                r = mid - 1;
+        }
+            if (k != arr[l])
+            {
+                printf("-1 -1\n");
+            }
+            else
+            {
+                printf("%d ", l);
+                int l = 0;
+                int r = n - 1;
+                while (l < r)
+                {
+                    int mid = (l + r) / 2;
+                    if (k <= arr[mid])
+                    {
+                        r = mid;
+                    }
+                    else
+                        l = mid + 1;
+                }
+                printf("%d ", l);
+            }
+    }
+    return 0;
 }
