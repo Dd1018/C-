c185631309522c038f1d4e1c9414fcfcab43dd39
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-12 22:17:08 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,89 +1,137 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-Swap(int* a, int* b)
+#include<stdlib.h>
+#include<assert.h>
+typedef int BTDataType;
+typedef struct BinaryTreeNode
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+BTNode* CreateTree()
+{
+	BTNode* n1 = (BTNode*)malloc(sizeof(BTNode));
+	assert(n1);
+	BTNode* n2 = (BTNode*)malloc(sizeof(BTNode));
+	assert(n2);
+	BTNode* n3 = (BTNode*)malloc(sizeof(BTNode));
+	assert(n3);
+	BTNode* n4 = (BTNode*)malloc(sizeof(BTNode));
+	assert(n4);
+	BTNode* n5 = (BTNode*)malloc(sizeof(BTNode));
+	assert(n5);
+	BTNode* n6 = (BTNode*)malloc(sizeof(BTNode));
+	assert(n6);
+
+	n1->data = 1;
+	n2->data = 2;
+	n3->data = 3;
+	n4->data = 4;
+	n5->data = 5;
+	n6->data = 6;
+
+	n1->left = n2;
+	n1->right = n4;
+	n2->left = n3;
+	n2->right = NULL;
+	n4->left = n5;
+	n4->right = n6;
+	n3->left = NULL;
+	n3->right = NULL;
+	n5->left = NULL;
+	n5->right = NULL;
+	n6->left = NULL;
+	n6->right = NULL;
+
+	n3->right = NULL;
+
+	return n1;
 }
-void BigAdjustDown(int *a,int n,int parent)
+// 二叉树前序遍历
+void PreOrder(BTNode* root)
 {
-	int minChild = parent * 2 + 1;
-	while (minChild < n)
+	if (root == NULL)
 	{
-		if (minChild + 1 < n && a[minChild + 1] > a[minChild])
-		{
-			minChild = minChild + 1;
-		}
-		if (a[minChild] > a[parent])
-		{
-			Swap(&a[minChild], &a[parent]);
-			parent = minChild;
-			minChild = parent * 2 + 1;
-		}
-		else
-			break;
+		printf("NULL ");
+		return;
 	}
+
+	printf("%d ", root->data);
+	PreOrder(root->left);
+	PreOrder(root->right);
 }
-void SmallAdjustDown(int* a, int n, int parent)
+
+// 二叉树中序遍历
+void InOrder(BTNode* root)
 {
-	int minChild = parent * 2 + 1;
-	while (minChild < n)
+	if (root == NULL)
 	{
-		if (minChild + 1 < n && a[minChild + 1] <a[minChild])
-		{
-			minChild = minChild + 1;
-		}
-		if (a[minChild]<a[parent])
-		{
-			Swap(&a[minChild], &a[parent]);
-			parent = minChild;
-			minChild = parent * 2 + 1;
-		}
-		else
-			break;
+		printf("NULL ");
+		return;
 	}
+
+	InOrder(root->left);
+	printf("%d ", root->data);
+	InOrder(root->right);
+}
+void PostOrder(BTNode* root)
+{
+	if (root == NULL)
+		return;
+	PostOrder(root->left);
+	PostOrder(root->right);
+	printf("%d ", root->data);
+}
+int TreeSize(BTNode* root)
+{
+	return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
+}
+int TreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	if (root->left == NULL && root->right == NULL)
+		return 1;
+	return TreeLeafSize(root->left)+
+	TreeLeafSize(root->right);
+}
+int TreeLevel(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+		return 0;
+	if (x == 1)
+		return 1;
+	return TreeLevel(root->left, x - 1) + TreeLevel(root->right, x - 1);
+}
+int TreeHeight(BTNode* root)
+{
+	if (root == NULL);
+	return 0;
+	int lh = TreeHeight(root->left);
+	int rh = TreeHeight(root->right);
+	return lh > rh ? lh + 1 : rh + 1;
+}
+BTNode* TreeFind(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+		return NULL;
+	if (root->data == x)
+		return root;
+	BTNode* lret = TreeFind(root->left, x);
+
+	if (lret)
+		return lret;
+	BTNode* rret = TreeFind(root->left, x);
+	if (rret)
+		return rret;
+	return NULL;
+
 }
 int main()
 {
-	int a[] = { 165,19869,123,46,26,165,213 };
-	int sz = sizeof(a) / sizeof(a[0]);
-	for (int i = (sz-2)/2; i >= 0; i--)
-	{
-		BigAdjustDown(a, sz, i);
-	}
-	int i = 1;
-	while (i < sz)
-	{
-		Swap(&a[0], &a[sz - i]);
-		BigAdjustDown(a, sz-i, 0);
-		++i;
-	}
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", a[i]);
-	}
-	printf("\n");
-	int b[] = { 165,654,18,123,4986,364 };
-	int k = 3;
-	 i = 0;
-	for (i = (k - 2) / 2; i >= 0; i--)
-	{
-		SmallAdjustDown(b, k, i);
-	}
-	for (i=3; i < 6; i++)
-	{
-		if (b[i] > b[0])
-		{
-	
-		b[0] = b[i];
-	
-			SmallAdjustDown(b, 6, 0);
-		}
-	}
-	for (i = 0; i < 6; i++)
-	{
-		printf("%d ", b[i]);
-	}
+	BTNode *BT= CreateTree();
+	int c=TreeLevel(BT, 3);
+	printf("%d", c);
 	return 0;
 }
