0c016806d9af44214b12f9e7d9b9391b0277ea8c
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-14 21:33:58 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,192 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-#include"Queue.h"
-BTNode* CreateTree()
+#include<string.h>
+void InsertSort(int* arr, int n)
 {
-	BTNode* n1 = (BTNode*)malloc(sizeof(BTNode));
-	assert(n1);
-	BTNode* n2 = (BTNode*)malloc(sizeof(BTNode));
-	assert(n2);
-	BTNode* n3 = (BTNode*)malloc(sizeof(BTNode));
-	assert(n3);
-	BTNode* n4 = (BTNode*)malloc(sizeof(BTNode));
-	assert(n4);
-	BTNode* n5 = (BTNode*)malloc(sizeof(BTNode));
-	assert(n5);
-	BTNode* n6 = (BTNode*)malloc(sizeof(BTNode));
-	assert(n6);
-
-	n1->data = 1;
-	n2->data = 2;
-	n3->data = 3;
-	n4->data = 4;
-	n5->data = 5;
-	n6->data = 6;
-
-	n1->left = n2;
-	n1->right = n3;
-	n2->left = n4;
-	n2->right = n5;
-	n4->left = NULL;
-	n4->right = NULL;
-	n3->left = NULL;
-	n3->right = n6;
-	n5->left = NULL;
-	n5->right = NULL;
-	n6->left = NULL;
-	n6->right = NULL;
-
-	return n1;
-}
-// 二叉树前序遍历
-void PreOrder(BTNode* root)
-{
-	if (root == NULL)
+	for (int i = 0; i < n-1; i++)
 	{
-		printf("NULL ");
-		return;
-	}
+		int end = i ;
 
-	printf("%d ", root->data);
-	PreOrder(root->left);
-	PreOrder(root->right);
-}
-
-// 二叉树中序遍历
-void InOrder(BTNode* root)
-{
-	if (root == NULL)
-	{
-		printf("NULL ");
-		return;
-	}
-
-	InOrder(root->left);
-	printf("%d ", root->data);
-	InOrder(root->right);
-}
-void PostOrder(BTNode* root)
-{
-	if (root == NULL)
-		return;
-	PostOrder(root->left);
-	PostOrder(root->right);
-	printf("%d ", root->data);
-}
-int TreeSize(BTNode* root)
-{
-	return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
-}
-int TreeLeafSize(BTNode* root)
-{
-	if (root == NULL)
-		return 0;
-	if (root->left == NULL && root->right == NULL)
-		return 1;
-	return TreeLeafSize(root->left)+
-	TreeLeafSize(root->right);
-}
-int TreeLevel(BTNode* root, BTDataType x)
-{
-	if (root == NULL)
-		return 0;
-	if (x == 1)
-		return 1;
-	return TreeLevel(root->left, x - 1) + TreeLevel(root->right, x - 1);
-}
-int TreeHeight(BTNode* root)
-{
-	if (root == NULL);
-	return 0;
-	int lh = TreeHeight(root->left);
-	int rh = TreeHeight(root->right);
-	return lh > rh ? lh + 1 : rh + 1;
-}
-BTNode* TreeFind(BTNode* root, BTDataType x)
-{
-	if (root == NULL)
-		return NULL;
-	if (root->data == x)
-		return root;
-	BTNode* lret = TreeFind(root->left, x);
-
-	if (lret)
-		return lret;
-	BTNode* rret = TreeFind(root->left, x);
-	if (rret)
-		return rret;
-	return NULL;
-
-}
-void BinaryTreeDestory(BTNode* root)
-{
-	if (root == NULL)
-		return;
-	BinaryTreeDestory(root->left);
-	BinaryTreeDestory(root->right);
-	free(root);
-}
-void TreeLevelOrder(BTNode* root) //遍历
-{
-	Queue q;
-	QueueInit(&q);
-	if (root)
-		QueuePush(&q, root);
-
-	while (!QueueEmpty(&q))
-	{
-		BTNode* front = QueueFront(&q);
-		QueuePop(&q);
-		printf("%d ", front->data);
-
-		// 下一层，入队列
-		if (front->left)
-			QueuePush(&q, front->left);
-
-		if (front->right)
-			QueuePush(&q, front->right);
-	}
-	printf("\n");
-
-	QueueDestroy(&q);
-}
-bool TreeLevelOrderComplete(BTNode* root) //遍历
-{
-	Queue q;
-	QueueInit(&q);
-	if (root)
-		QueuePush(&q, root);
-
-	while (!QueueEmpty(&q))
-	{
-		BTNode* front = QueueFront(&q);
-		QueuePop(&q);
-		if (front == NULL)
-			break;
-			QueuePush(&q, front->left);
-			QueuePush(&q, front->right);
-	}
-	while (!QueueEmpty(&q))
-	{
-		BTNode* front = QueueFront(&q);
-		QueuePop(&q);
-		if (front != NULL)
+		int key = arr[end+1];
+		while (end >= 0)
 		{
-			QueueDestroy(&q);
-			return false;
+			if (key < arr[end])
+			{
+				arr[end+1] = arr[end];
+				end--;
+			}
+			else
+				break;
 		}
+		arr[end+ 1] = key;
 	}
-
-	QueueDestroy(&q);
-	return true;
 }
 int main()
 {
-	BTNode *BT= CreateTree();
-	TreeLevelOrder(BT);
-	if (TreeLevelOrderComplete(BT))
-		printf("1");
+	int arr[] = { 165, 132, 13, 165, 132, 165, 132, 165, 132, 165, 132, 165 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	InsertSort(arr, n);
 	return 0;
 }
