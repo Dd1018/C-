c81e4664eba9b7a9611238757b4b0c5ca13ba93f
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-15 11:51:27 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,30 +1,195 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-void InsertSort(int* arr, int n)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void Swap(int* p1, int* p2)
 {
-	for (int i = 0; i < n-1; i++)
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+void BubbleSort(int* a, int n)
+{
+	for (int j = 0; j < n; ++j)
 	{
-		int end = i ;
-
-		int key = arr[end+1];
-		while (end >= 0)
+		int exchange = 0;
+		for (int i = 1; i < n - j; ++i)
 		{
-			if (key < arr[end])
+			if (a[i - 1] > a[i])
 			{
-				arr[end+1] = arr[end];
-				end--;
+				Swap(&a[i - 1], &a[i]);
+				exchange = 1;
 			}
-			else
-				break;
 		}
-		arr[end+ 1] = key;
+		if (exchange == 0)
+		{
+			break;
+		}
 	}
+}
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;//或gap=gap/2;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (a[end] > tmp)
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}
+}
+// 最坏时间复杂度：O(N^2)
+// 最好时间复杂度：O(N^2)
+void SelectSort(int* a, int n)
+{
+	int begin = 0, end = n - 1;
+	while (begin < end)
+	{
+		// 选出最小的放begin位置
+		// 选出最大的放end位置
+		int mini = begin, maxi = begin;
+		for (int i = begin + 1; i <= end; ++i)
+		{
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+
+		Swap(&a[begin], &a[mini]);
+		// 修正一下maxi
+		if (maxi == begin)
+			maxi = mini;
+
+		Swap(&a[end], &a[maxi]);
+		++begin;
+		--end;
+	}
+}
+void TestOP()
+{
+	srand(time(0));
+	const int N = 100000;
+	int* a1 = (int*)malloc(sizeof(int) * N);
+	int* a2 = (int*)malloc(sizeof(int) * N);
+	int* a3 = (int*)malloc(sizeof(int) * N);
+	int* a4 = (int*)malloc(sizeof(int) * N);
+	int* a5 = (int*)malloc(sizeof(int) * N);
+	int* a6 = (int*)malloc(sizeof(int) * N);
+	int* a7 = (int*)malloc(sizeof(int) * N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		a1[i] = rand();
+		a2[i] = a1[i];
+		a3[i] = a1[i];
+		a4[i] = a1[i];
+		a5[i] = a1[i];
+		a6[i] = a1[i];
+		a7[i] = a1[i];
+	}
+
+	int begin1 = clock();
+	//InsertSort(a1, N);
+	int end1 = clock();
+
+	int begin2 = clock();
+	ShellSort(a2, N);
+	int end2 = clock();
+
+	int begin3 = clock();
+	SelectSort(a3, N);
+	int end3 = clock();
+
+	int begin4 = clock();
+	//HeapSort(a4, N);
+	int end4 = clock();
+
+	int begin5 = clock();
+	//QuickSort(a5, 0, N - 1);
+	int end5 = clock();
+
+	int begin6 = clock();
+	//MergeSort(a6, N);
+	int end6 = clock();
+
+	int begin7 = clock();
+	//BubbleSort(a7, N);
+	int end7 = clock();
+
+	printf("InsertSort:%d\n", end1 - begin1);
+	printf("ShellSort:%d\n", end2 - begin2);
+	printf("SelectSort:%d\n", end3 - begin3);
+	printf("HeapSort:%d\n", end4 - begin4);
+	printf("BubbleSort:%d\n", end7 - begin7);
+	printf("QuickSort:%d\n", end5 - begin5);
+	printf("MergeSort:%d\n", end6 - begin6);
+
+
+	free(a1);
+	free(a2);
+	free(a3);
+	free(a4);
+	free(a5);
+	free(a6);
+	free(a7);
+}
+int PartSort(int* arr, int left, int right)
+{
+	int keyi = left;
+	while (left < right)
+	{
+		while (left < right && arr[left] <= arr[keyi])
+		{
+			left++;
+		}
+		while (left < right && arr[right] <= arr[right])//left<right是为了防止这俩个错过
+		{
+			right--;
+		}
+		if (left == right)
+			Swap(&arr[left], &arr[keyi]);
+	}
+	int meeti;
+	if (left == right)
+	 meeti = left;
+		Swap(&arr[meeti], &arr[right]);
+		return meeti;
+}
+void QuickSort(int* a, int begin, int end)
+{
+	if (begin >= end)
+	{
+		return;
+	}
+	int keyi=PartSort(a, begin, end);
+	QuickSort(a, begin, keyi - 1);//
+	QuickSort(a, keyi+1, end);
 }
 int main()
 {
-	int arr[] = { 165, 132, 13, 165, 132, 165, 132, 165, 132, 165, 132, 165 };
+	int arr[] = {9,1,2,5,7,4,8,6,3,5 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	InsertSort(arr, n);
+	QuickSort(arr, 0, n - 1);
 	return 0;
 }
