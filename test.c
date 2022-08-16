fb1d2e85685ef5d32f793554963e2b7cf70a52f6
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-16 21:49:16 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,180 +1,122 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include"Stack.h"
 void Swap(int* p1, int* p2)
 {
 	int tmp = *p1;
 	*p1 = *p2;
 	*p2 = tmp;
 }
-void BubbleSort(int* a, int n)
+int GetMindIndex(int* arr, int left, int right)
 {
-	for (int j = 0; j < n; ++j)
+	int mid = (right + left) / 2;
+	if (arr[left] < arr[mid])
 	{
-		int exchange = 0;
-		for (int i = 1; i < n - j; ++i)
-		{
-			if (a[i - 1] > a[i])
-			{
-				Swap(&a[i - 1], &a[i]);
-				exchange = 1;
-			}
-		}
-		if (exchange == 0)
-		{
-			break;
-		}
+		if (arr[mid] < arr[right])
+			return mid;
+		else if (arr[right]<arr[left])
+			return left;
+		else
+			return right;
+	}
+	else
+	{
+		if (arr[mid] > arr[right])
+			return mid;
+		else if (arr[right] > arr[left])
+			return left;
+		else
+			return right;
 	}
 }
-void ShellSort(int* a, int n)
+int PartSort(int* arr, int left, int right)
 {
-	int gap = n;
-	while (gap > 1)
+	int mid = GetMindIndex(arr, left, right);
+	Swap(&arr[mid], &arr[left]);
+	int keyi = left;
+	while (left < right)
 	{
-		gap = gap / 3 + 1;//或gap=gap/2;
-		for (int i = 0; i < n - gap; i++)
+		while (left < right && arr[right] >= arr[keyi])//left<right是为了防止这俩个错过
+		{
+			right--;
+		}
+		while (left < right && arr[left] <= arr[keyi])
+		{
+			left++;
+		}
+		
+		if (left<right)
+			Swap(&arr[left], &arr[right]);
+	}
+	int meeti = left;
+	Swap(&arr[meeti], &arr[keyi]);
+	return meeti;
+}
+void InsertSort(int* a, int n)
+{
+
+		for (int i = 0; i < n -1; i++)
 		{
 			int end = i;
-			int tmp = a[end + gap];
+			int tmp = a[end +1];
 			while (end >= 0)
 			{
 				if (a[end] > tmp)
 				{
-					a[end + gap] = a[end];
-					end -= gap;
+					a[end +1] = a[end];
+					end --;
 				}
 				else
 				{
 					break;
 				}
 			}
-			a[end + gap] = tmp;
+			a[end +1] = tmp;
 		}
-	}
 }
-// 最坏时间复杂度：O(N^2)
-// 最好时间复杂度：O(N^2)
-void SelectSort(int* a, int n)
+
+//挖坑法
+int PartSort2(int* arr, int left, int right)
 {
-	int begin = 0, end = n - 1;
-	while (begin < end)
-	{
-		// 选出最小的放begin位置
-		// 选出最大的放end位置
-		int mini = begin, maxi = begin;
-		for (int i = begin + 1; i <= end; ++i)
-		{
-			if (a[i] > a[maxi])
-			{
-				maxi = i;
-			}
-
-			if (a[i] < a[mini])
-			{
-				mini = i;
-			}
-		}
-
-		Swap(&a[begin], &a[mini]);
-		// 修正一下maxi
-		if (maxi == begin)
-			maxi = mini;
-
-		Swap(&a[end], &a[maxi]);
-		++begin;
-		--end;
-	}
-}
-void TestOP()
-{
-	srand(time(0));
-	const int N = 100000;
-	int* a1 = (int*)malloc(sizeof(int) * N);
-	int* a2 = (int*)malloc(sizeof(int) * N);
-	int* a3 = (int*)malloc(sizeof(int) * N);
-	int* a4 = (int*)malloc(sizeof(int) * N);
-	int* a5 = (int*)malloc(sizeof(int) * N);
-	int* a6 = (int*)malloc(sizeof(int) * N);
-	int* a7 = (int*)malloc(sizeof(int) * N);
-
-	for (int i = 0; i < N; ++i)
-	{
-		a1[i] = rand();
-		a2[i] = a1[i];
-		a3[i] = a1[i];
-		a4[i] = a1[i];
-		a5[i] = a1[i];
-		a6[i] = a1[i];
-		a7[i] = a1[i];
-	}
-
-	int begin1 = clock();
-	//InsertSort(a1, N);
-	int end1 = clock();
-
-	int begin2 = clock();
-	ShellSort(a2, N);
-	int end2 = clock();
-
-	int begin3 = clock();
-	SelectSort(a3, N);
-	int end3 = clock();
-
-	int begin4 = clock();
-	//HeapSort(a4, N);
-	int end4 = clock();
-
-	int begin5 = clock();
-	//QuickSort(a5, 0, N - 1);
-	int end5 = clock();
-
-	int begin6 = clock();
-	//MergeSort(a6, N);
-	int end6 = clock();
-
-	int begin7 = clock();
-	//BubbleSort(a7, N);
-	int end7 = clock();
-
-	printf("InsertSort:%d\n", end1 - begin1);
-	printf("ShellSort:%d\n", end2 - begin2);
-	printf("SelectSort:%d\n", end3 - begin3);
-	printf("HeapSort:%d\n", end4 - begin4);
-	printf("BubbleSort:%d\n", end7 - begin7);
-	printf("QuickSort:%d\n", end5 - begin5);
-	printf("MergeSort:%d\n", end6 - begin6);
-
-
-	free(a1);
-	free(a2);
-	free(a3);
-	free(a4);
-	free(a5);
-	free(a6);
-	free(a7);
-}
-int PartSort(int* arr, int left, int right)
-{
-	int keyi = left;
+	int mid = GetMindIndex(arr, left, right);
+	Swap(&arr[mid], &arr[left]);
+	int hole = left;
+	int key = arr[left];
 	while (left < right)
 	{
-		while (left < right && arr[left] <= arr[keyi])
-		{
-			left++;
-		}
-		while (left < right && arr[right] <= arr[right])//left<right是为了防止这俩个错过
+		while (left < right && arr[right] >= key)
 		{
 			right--;
 		}
-		if (left == right)
-			Swap(&arr[left], &arr[keyi]);
+		arr[hole]=arr[right];
+		hole = right;
+		while (left < right && arr[left]<= key)
+		{
+			left++;
+		}
+		arr[hole] = arr[left];
+		hole = left;
 	}
-	int meeti;
-	if (left == right)
-	 meeti = left;
-		Swap(&arr[meeti], &arr[right]);
-		return meeti;
+	arr[hole] = key;
+	return hole;
+}
+int PartSort3(int* arr, int left, int right)
+{
+	int mid = GetMindIndex(arr, left, right);
+	Swap(&arr[mid], &arr[left]);
+	int keyi = left;
+	int cur = left + 1;
+	int prev = left;
+	while (cur <= right)
+	{
+		if (arr[cur] < arr[keyi] && ++prev != cur)//等于可以不用动那个值，让他留在哪里
+		{
+			Swap(&arr[cur], &arr[prev]);
+		}
+		++cur;
+	}
+	Swap(&arr[keyi], &arr[prev]);
+	return prev;
 }
 void QuickSort(int* a, int begin, int end)
 {
@@ -182,14 +124,63 @@ void QuickSort(int* a, int begin, int end)
 	{
 		return;
 	}
-	int keyi=PartSort(a, begin, end);
-	QuickSort(a, begin, keyi - 1);//
-	QuickSort(a, keyi+1, end);
+	if (end - begin <= 8)
+	{
+		InsertSort(a + begin, end - begin + 1);
+	}
+	else
+	{
+		int keyi = PartSort3(a, begin, end);
+		QuickSort(a, begin, keyi - 1);//
+		QuickSort(a, keyi + 1, end);
+	}
+
 }
+
+void QuickSortNonR(int* a, int begin, int end)
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, begin);
+	StackPush(&st, end);
+
+	while (!StackEmpty(&st))
+	{
+		int right = StackTop(&st);
+		StackPop(&st);
+
+		int left = StackTop(&st);
+		StackPop(&st);
+
+		/*if (left >= right)
+		{
+			continue;
+		}*/
+
+		int keyi = PartSort3(a, left, right);
+		// [left, keyi-1] keyi [keyi+1,right]
+
+		if (keyi + 1 < right)
+		{
+			StackPush(&st, keyi + 1);
+			StackPush(&st, right);
+		}
+
+		if (left < right - 1)
+		{
+			StackPush(&st, left);
+			StackPush(&st, keyi - 1);
+		}
+	}
+
+	StackDestory(&st);
+}
+
+
 int main()
 {
-	int arr[] = {9,1,2,5,7,4,8,6,3,5 };
+	int arr[] = {1,2,3,4,5,6,7,8,9,10 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	QuickSort(arr, 0, n - 1);
+	QuickSortNonR(arr, 0, n - 1);
 	return 0;
 }
