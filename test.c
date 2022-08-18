9b4ea00d845250c929892ea94c9587391a30952b
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-18 21:49:49 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,186 +1,194 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include"Stack.h"
-void Swap(int* p1, int* p2)
+#include<stdlib.h>
+#include<string.h>
+void _MergeSort(int* a, int begin, int end, int* tmp)
 {
-	int tmp = *p1;
-	*p1 = *p2;
-	*p2 = tmp;
-}
-int GetMindIndex(int* arr, int left, int right)
-{
-	int mid = (right + left) / 2;
-	if (arr[left] < arr[mid])
+	if (begin >= end)
+		return;
+	int mid = (begin + end) / 2;
+	_MergeSort(a, begin, mid, tmp);
+	_MergeSort(a, mid+1, end, tmp);
+	int begin1 = begin; int end1 = mid;
+	int i = begin1;
+	int begin2 = mid + 1; int end2 = end;
+	while (begin1 <= end1 && begin2 <= end2)
 	{
-		if (arr[mid] < arr[right])
-			return mid;
-		else if (arr[right]<arr[left])
-			return left;
+		if (a[begin1] < a[begin2])
+			tmp[i++] = a[begin1++];
 		else
-			return right;
+			tmp[i++] = a[begin2++];
 	}
-	else
-	{
-		if (arr[mid] > arr[right])
-			return mid;
-		else if (arr[right] > arr[left])
-			return left;
-		else
-			return right;
-	}
+	while (begin1 <= end1)
+		tmp[i++] = a[begin1++];
+	while (begin2<= end2)
+		tmp[i++] = a[begin2++];
+	memcpy(a + begin, tmp + begin, ((end - begin) + 1) * sizeof(int));
 }
-int PartSort(int* arr, int left, int right)
+void MergeSort(int* a, int n)
 {
-	int mid = GetMindIndex(arr, left, right);
-	Swap(&arr[mid], &arr[left]);
-	int keyi = left;
-	while (left < right)
-	{
-		while (left < right && arr[right] >= arr[keyi])//left<right是为了防止这俩个错过
-		{
-			right--;
-		}
-		while (left < right && arr[left] <= arr[keyi])
-		{
-			left++;
-		}
-		
-		if (left<right)
-			Swap(&arr[left], &arr[right]);
-	}
-	int meeti = left;
-	Swap(&arr[meeti], &arr[keyi]);
-	return meeti;
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	if (tmp == NULL)
+		return;
+	_MergeSort(a, 0, n - 1, tmp);
+	free(tmp);
 }
-void InsertSort(int* a, int n)
+void PrintArray(int* a, int n)
 {
-
-		for (int i = 0; i < n -1; i++)
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+void _MergeSort1(int* a, int n)
+{
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	if (tmp == NULL)
+		return;
+	int gap = 1;
+	while (gap < n)
+	{
+		for (int i = 0; i < n; i = i + 2 * gap)
 		{
-			int end = i;
-			int tmp = a[end +1];
-			while (end >= 0)
+			int begin1 = i; int end1 = i + gap - 1;
+			int begin2 = i + gap; int end2 = i + 2 * gap - 1;
+			int j = begin1;
+			if (end1 >= n)
 			{
-				if (a[end] > tmp)
+				end1 = n - 1;
+				begin2 = n;
+				end2 = n - 1;
+			}
+			else if (begin2 >= n)
+			{
+				begin2 = n;
+				end2 = n - 1;
+			}
+			else if (end2 >= n)
+			{
+				end2 = n - 1;
+			}
+			while (begin1 <= end1 && begin2 <= end2)
+			{
+				if (a[begin1] < a[begin2])
+					tmp[j++] = a[begin1++];
+				else
+					tmp[j++] = a[begin2++];
+			}
+			while (begin1 <= end1)
+				tmp[j++] = a[begin1++];
+			while (begin2 <= end2)
+				tmp[j++] = a[begin2++];
+		
+	
+		}
+		memcpy(a, tmp, n * sizeof(int));
+		gap = gap * 2;
+	}
+	PrintArray(a, 10);
+}
+void MergeSortNonR(int* a, int n)
+{
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	if (tmp == NULL)
+	{
+		printf("malloc fail\n");
+		exit(-1);
+	}
+	int gap = 1;
+	while (gap < n)
+	{
+		//printf("gap=%d->", gap);
+		for (int i = 0; i < n; i += 2 * gap)
+		{
+			// [i,i+gap-1][i+gap, i+2*gap-1]
+			int begin1 = i, end1 = i + gap - 1;
+			int begin2 = i + gap, end2 = i + 2 * gap - 1;
+
+			// end1越界或者begin2越界，则可以不归并了
+			if (end1 >= n || begin2 >= n)
+			{
+				break;
+			}
+			else if (end2 >= n)
+			{
+				end2 = n - 1;
+			}
+			//printf("[%d,%d] [%d, %d]--", begin1, end1, begin2, end2);
+
+			int m = end2 - begin1 + 1;
+			int j = begin1;
+			while (begin1 <= end1 && begin2 <= end2)
+			{
+				if (a[begin1] < a[begin2])
 				{
-					a[end +1] = a[end];
-					end --;
+					tmp[j++] = a[begin1++];
 				}
 				else
 				{
-					break;
+					tmp[j++] = a[begin2++];
 				}
 			}
-			a[end +1] = tmp;
-		}
-}
 
-//挖坑法
-int PartSort2(int* arr, int left, int right)
-{
-	int mid = GetMindIndex(arr, left, right);
-	Swap(&arr[mid], &arr[left]);
-	int hole = left;
-	int key = arr[left];
-	while (left < right)
-	{
-		while (left < right && arr[right] >= key)
-		{
-			right--;
+			while (begin1 <= end1)
+			{
+				tmp[j++] = a[begin1++];
+			}
+
+			while (begin2 <= end2)
+			{
+				tmp[j++] = a[begin2++];
+			}
+
+			memcpy(a + i, tmp + i, sizeof(int) * m);
 		}
-		arr[hole]=arr[right];
-		hole = right;
-		while (left < right && arr[left]<= key)
-		{
-			left++;
-		}
-		arr[hole] = arr[left];
-		hole = left;
+
+		gap *= 2;
 	}
-	arr[hole] = key;
-	return hole;
+
+	free(tmp);
 }
-int PartSort3(int* arr, int left, int right)
+void MergeSort1(int* a, int n)
 {
-	int mid = GetMindIndex(arr, left, right);
-	Swap(&arr[mid], &arr[left]);
-	int keyi = left;
-	int cur = left + 1;
-	int prev = left;
-	while (cur <= right)
+
+	_MergeSort1(a, n);
+}
+void CountSort(int *a,int n)
+{
+	int min = a[0], max = a[0];//先找到最大值和最小值
+	for (int i = 0; i < n; i++)
 	{
-		if (arr[cur] < arr[keyi] && ++prev != cur)//等于可以不用动那个值，让他留在哪里
+		if (a[i] < min)
 		{
-			Swap(&arr[cur], &arr[prev]);
+			min = a[i];
 		}
-		++cur;
+		if (a[i] >max)
+		{
+			max = a[i];
+		}
 	}
-	Swap(&arr[keyi], &arr[prev]);
-	return prev;
-}
-void QuickSort(int* a, int begin, int end)
-{
-	if (begin >= end)
-	{
+	int range = max - min + 1;//算出范围
+	int* count =(int*) malloc(sizeof(int) * range);
+	if (count == NULL)
 		return;
-	}
-	if (end - begin <= 8)
+	memset(count, 0, sizeof(int) * range);
+	for (int i = 0; i < n; ++i)
 	{
-		InsertSort(a + begin, end - begin + 1);
+		count[a[i] - min]++;//统计次数
 	}
-	else
+	//排序
+	int j = 0;
+	for (int i = 0; i < range; ++i)
 	{
-		int keyi = PartSort3(a, begin, end);
-		QuickSort(a, begin, keyi - 1);//
-		QuickSort(a, keyi + 1, end);
+		//出现几次，回写几个i+min
+		while (count[i]--)
+		{
+			a[j++] = i + min;
 	}
-
+	}
 }
-
-void QuickSortNonR(int* a, int begin, int end)
-{
-	ST st;
-	StackInit(&st);
-	StackPush(&st, begin);
-	StackPush(&st, end);
-
-	while (!StackEmpty(&st))
-	{
-		int right = StackTop(&st);
-		StackPop(&st);
-
-		int left = StackTop(&st);
-		StackPop(&st);
-
-		/*if (left >= right)
-		{
-			continue;
-		}*/
-
-		int keyi = PartSort3(a, left, right);
-		// [left, keyi-1] keyi [keyi+1,right]
-
-		if (keyi + 1 < right)
-		{
-			StackPush(&st, keyi + 1);
-			StackPush(&st, right);
-		}
-
-		if (left < right - 1)
-		{
-			StackPush(&st, left);
-			StackPush(&st, keyi - 1);
-		}
-	}
-
-	StackDestory(&st);
-}
-
-
 int main()
 {
-	int arr[] = {1,2,3,4,5,6,7,8,9,10 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	QuickSortNonR(arr, 0, n - 1);
+	int a[10] = {10,6,7,1,3,9,4,2,10,11 };
+	CountSort(a,10);
 	return 0;
 }
