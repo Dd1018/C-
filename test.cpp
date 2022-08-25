c7aa5b30d126b35b8d56e9a2e1a59414f04b5c71
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-25 21:45:59 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,49 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-
 using namespace std;
-#include"F:\code\StackC\Stack.h"
-bool isValid(const char* s) {
-	ST st;
-	StackInit(&st);
-	while (*s)
+class Date
+{
+public:
+	Date(int year, int month , int day)
 	{
-		if (*s == '(' || *s == '[' || *s == '{')
-		{
-			StackPush(&st, *s);
-			++s;
-		}
-		else
-		{
-			if (StackEmpty(&st))
-			{
-				StackDestroy(&st);
-				return false;
-			}
-
-			STDataType top = StackTop(&st);
-			StackPop(&st);
-			if ((top == '{' && *s == '}')
-				|| (top == '[' && *s == ']')
-				|| (top == '(' && *s == ')'))
-			{
-				++s;
-			}
-			else
-			{
-				StackDestroy(&st);
-				return false;
-			}
-		}
+		_year = year;
+		_month = month;
+		_day = day;
 	}
 
-	bool ret = StackEmpty(&st);
-	StackDestroy(&st);
-	return ret;
-}
+	void Print()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
 
+private:
+	int _year ;   // 注意这里不是初始化，给缺省值
+	int _month ;
+	int _day ;
+};
+typedef int DataType;
+class Stack
+{
+public:
+	Stack(int capacity=4)
+	{
+		cout << "Stack(int capacity = 4)" << endl;
+		_array = (DataType*)malloc(sizeof(DataType) * capacity);
+		if (NULL == _array)
+		{
+			perror("malloc申请空间失败!!!");
+			return;
+		}
+
+		_size = 0;
+		_capacity = capacity;
+	}
+
+	void Push(DataType data)
+	{
+		// CheckCapacity();
+		_array[_size] = data;
+		_size++;
+	}
+private:
+	DataType* _array;
+	int _capacity;
+	int _size;
+};
+
+// C++类型分类：
+// 内置类型/基本类型：int/double/char/指针等等 
+// 自定义类型：struct/class
+
+class MyQueue
+{
+private:
+	Stack _st1;
+	Stack _st2;
+};
 int main()
 {
-	cout << isValid("{}{}{}") << endl;
+	Date a;
 	return 0;
 }
