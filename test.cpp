942a8aa5ff5cffa243f72ec9765899f741857d81
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-28 21:17:13 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Date
 {
 public:
+
 	Date(int year = 1, int month = 1, int day = 1)
 	{
 		_year = year;
@@ -20,6 +21,43 @@ public:
 	void Print()
 	{
 		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+	bool operator==(const Date& x2)//判断是否相等
+	{
+		return _year == x2._year &&
+			_month == x2._month &&
+			_day == x2._day;
+	}
+	int GetMonthDay(int year, int month)
+	{
+		static int days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		//多次调用的时候，每次都要创建数组，现在改为静态，放到静态区
+
+		if (month == 2
+			&& ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))//闰年2月
+		{
+			return 29;
+		}
+		else
+		{
+			return days[month];
+		}
+	}
+	Date operator+(int day)
+	{
+		Date ret(*this);
+		ret._day += day;
+		while (ret._day > GetMonthDay(ret._year,ret._month))
+		{
+			ret._day -= GetMonthDay(ret._year,ret. _month);
+			++ret._month;
+			if (ret._month == 13)//12月特殊处理
+			{
+				ret._month = 1;
+				ret._year++;
+			}
+		}
+		return ret;
 	}
 private:
 	int _year = 1;   // 注意这里不是初始化，给缺省值
@@ -56,6 +94,7 @@ public:
 		_capacity = _size = 0;
 		_array = nullptr;
 	}
+
 private:
 	DataType* _array;
 	int _capacity;
@@ -81,8 +120,11 @@ void fun()
 	Stack st;
 	Stack st1(st);
 }
+
 int main()
 {
-	fun();
+	Date d1(2022,7,23);
+	Date ret = (d1 + 50);
+	Date ret1(d1 + 50);
 	return 0;
 }
