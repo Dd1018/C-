350171b8e3342b2d896edb397c6a541c4fecd447
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-08-30 13:39:44 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -4,127 +4,54 @@ using namespace std;
 class Date
 {
 public:
-
-	Date(int year = 1, int month = 1, int day = 1)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-		cout << "构造函数" << endl;
-	}
-	//Date(const Date& d)//拷贝构造函数
+	//Date* operator&()
 	//{
-	//	_year = d._year;
-	//	_month = d._month;
-	//	_day = d._day;
+	//	return this;
 	//}
+	//const Date* operator&()const
+	//{
+	//	return this;
+	//}
+	void Print()const
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
 	void Print()
 	{
-		cout << _year << "-" << _month << "-" << _day << endl;
+		cout << _year << "/" << _month << "/" << _day << endl;
 	}
-	bool operator==(const Date& x2)//判断是否相等
+	Date()
 	{
-		return _year == x2._year &&
-			_month == x2._month &&
-			_day == x2._day;
-	}
-	int GetMonthDay(int year, int month)
-	{
-		static int days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		//多次调用的时候，每次都要创建数组，现在改为静态，放到静态区
 
-		if (month == 2
-			&& ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))//闰年2月
-		{
-			return 29;
-		}
-		else
-		{
-			return days[month];
-		}
-	}
-	Date operator+(int day)
-	{
-		Date ret(*this);
-		ret._day += day;
-		while (ret._day > GetMonthDay(ret._year,ret._month))
-		{
-			ret._day -= GetMonthDay(ret._year,ret. _month);
-			++ret._month;
-			if (ret._month == 13)//12月特殊处理
-			{
-				ret._month = 1;
-				ret._year++;
-			}
-		}
-		return ret;
 	}
 private:
-	int _year = 1;   // 注意这里不是初始化，给缺省值
-	int _month = 1;
-	int _day = 1;
+	int _year; // 年
+	int _month; // 月
+	int _day; // 日
 };
-typedef int DataType;
-class Stack
+int i = 1;
+int sum = 0;
+class Sum
 {
 public:
-	Stack(int capacity=4)
+	Sum()
 	{
-		cout << "Stack(int capacity = 4)" << endl;
-		_array = (DataType*)malloc(sizeof(DataType) * capacity);
-		if (NULL == _array)
-		{
-			perror("malloc申请空间失败!!!");
-			return;
-		}
-		_size = 0;
-		_capacity = capacity;
+		sum += i;
+		++i;
 	}
-
-	void Push(DataType data)
-	{
-		// CheckCapacity();
-		_array[_size] = data;
-		_size++;
-	}
-	~Stack()
-	{
-		cout << "~Stack()" << endl;
-		free(_array);
-		_capacity = _size = 0;
-		_array = nullptr;
-	}
-
-private:
-	DataType* _array;
-	int _capacity;
-	int _size;
 };
-class MyQueue
+class A
 {
-private:
-	int _size;
-	Stack _st1;
-	Stack _st2;
+public:
+	int Sum_A(int n)
+	{
+		Sum a[10];
+		return sum;
+	}
 };
-void get1(Date d)//d1的临时拷贝
-{
-
-}
-void get2(Date& d)
-{
-
-}
-void fun()
-{
-	Stack st;
-	Stack st1(st);
-}
-
 int main()
 {
-	Date d1(2022,7,23);
-	Date ret = (d1 + 50);
-	Date ret1(d1 + 50);
+	A s1;
+	s1.Sum_A(10);
 	return 0;
 }
