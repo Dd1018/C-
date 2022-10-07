84db62d2f2c12d84bad022cc8f6569a3588f36ea
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-10-07 21:09:52 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,114 +1,74 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include<string>
 #include<iostream>
-#include<vector>
-
 using namespace std;
-//void test1()
+//class Person
 //{
-//	vector<int> v1;
-//	vector<int> v2(10, 1);
-//	vector<int> v3(v2);
-//	v1.push_back(1);
-//	v1.push_back(2);
-//	v1.push_back(3);
-//	v1.push_back(4);
-//	v1.push_back(5);
-//	v1.push_back(6);
-//	for (size_t i = 0; i < v1.size(); ++i)
+//public:
+//	void Print()
 //	{
-//		v1[i]++;
-//		cout << v1[i] << " ";
+//		cout << "name:" << _name << endl;
+//		cout << "age:" << _age << endl;
 //	}
-//	cout << endl;
-//	vector<int>::iterator it = v1.begin();
-//	while (it != v1.end())
-//	{
-//		cout << *it << " ";
-//		it++;
-//	}
-//	cout << endl;
-//	for (auto ch : v1)
-//	{
-//		cout << ch << " ";
-//	}
-//	cout << endl;
-//	vector<int>::iterator pos = find(v1.begin(), v1.end(), 3);
-//	if (pos != v1.end())
-//	{
-//		v1.insert(pos, 30);
-//	}
-//	for (auto ch : v1)
-//	{
-//		cout << ch << " ";
-//	}
-//	cout << endl;
-//}
-//#include <algorithm>
-//void test2()
+//protected:
+//	string _name = "peter"; // 姓名
+//	int _age = 18; // 年龄
+//};
+//class Student :public Person
 //{
-//	vector<int>v1;
-//	v1.push_back(54);
-//	v1.push_back(24);
-//	v1.push_back(58);
-//	v1.push_back(12);
-//	v1.push_back(69);
-//	v1.push_back(36);
-//	v1.push_back(29);
-//	v1.push_back(25);
-//	v1.push_back(100);
-//	sort(v1.begin(), v1.end());
-//	for (auto ch : v1)
+//public:
+//	void Set(const char* name, int age)
 //	{
-//		cout << ch << " ";
+//		_name = name;
+//		_age = age;
 //	}
-//	cout << endl;
-//	less<int> ls;
-//	greater<int>gt;
-//	sort(v1.begin(), v1.end(),gt);
-//	for (auto ch : v1)
-//	{
-//		cout << ch << " ";
-//	}
-//	cout << endl;
-//	string s("hello31313131244");
-//	sort(s.begin(), s.end());
-//	for (auto ch : s)
-//	{
-//		cout << ch << " ";
-//	}
-//	cout << endl;
-//	sort(s.begin(), s.end(),greater<char>());
-//	for (auto ch : s)
-//	{
-//		cout << ch << " ";
-//	}
-//	cout << endl;
-//}
-class Date
+//protected:
+//	int _stuid;//学号
+//
+//};
+// Student的_num和Person的_num构成隐藏关系，可以看出这样代码虽然能跑，但是非常容易混淆
+class Person
+{
+protected:
+	string _name = "小李子"; // 姓名
+	int _num = 111; // 身份证号
+};
+class Student : public Person
 {
 public:
-	string reverseStr(string s, int k) {
-		int n = s.size() / 2 * k;
-		int i;
-		while (n)
-		{
-			int j = 0;
-			for (i = j; i < 4; ++i)
-			{
-				;
-			}
-			reverse(s[i - 4], s[i - 3]);
-			j += 4;
-			n--;
-		}
-		return s;
+	void Print()
+	{
+		cout << " 姓名:" << _name << endl;
+		cout << " 身份证号:" << Person::_num << endl;
+		cout << " 学号:" << _num << endl;
 	}
+protected:
+	int _num = 999; // 学号
+};
+class A
+{
+public:
+	void fun()
+	{
+		cout << "func()" << endl;
+	}
+};
+class B : public A
+{
+public:
+	void fun(int i)
+	{
+		A::fun();
+		cout << "func(int i)->" << i << endl;
+	}
+};
+void Test()
+{
+	B b;
+	b.A::fun();
 };
 int main()
 {
-	Date s;
-	s.reverseStr("abcdefg", 2);
-	string s1;
-	s1.reserve
+	Test();
 	return 0;
 }
