ddb59645d8cb715f371dbea432ace2b1263cc102
Dd1018 <100122612+Dd1018@users.noreply.github.com>
2022-10-24 21:49:28 +0800

    Add files via upload
    二叉搜索树

--- test.cpp
+++ test.cpp
@@ -1,310 +1,537 @@
-Ôªø#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <string>
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
 using namespace std;
-//
-//class Person
-//{
-//public:
-//	//Person(const char* name = "peter")
-//	Person(const char* name)
-//		: _name(name)
-//	{
-//		cout << "Person()" << endl;
-//	}
-//
-//	Person(const Person& p)
-//		: _name(p._name)
-//	{
-//		cout << "Person(const Person& p)" << endl;
-//	}
-//
-//	Person& operator=(const Person& p)
-//	{
-//		cout << "Person operator=(const Person& p)" << endl;
-//		if (this != &p)
-//			_name = p._name;
-//
-//		return *this;
-//	}
-//
-//	~Person()
-//	{
-//		cout << "~Person()" << endl;
-//	}
-//protected:
-//	string _name; // ÂßìÂêç
-//};
-//
-//class Student : public Person
-//{
-//public:
-//	Student(const char* name, int num)
-//		:Person(name)
-//		, _num(num)
-//	{
-//		cout << "Student()" << endl;
-//	}
-//
-//	Student(const Student& s)
-//		:Person(s)
-//		, _num(s._num)
-//	{
-//		cout << "Student(const Student& s)" << endl;
-//	}
-//
-//	Student& operator=(const Student& s)
-//	{
-//		if (this != &s)
-//		{
-//			Person::operator=(s);
-//
-//			_num = s._num;
-//		}
-//
-//		cout << "Student& operator=(const Student& s)" << endl;
-//
-//		return *this;
-//	}
-//
-//protected:
-//	int _num;
-//};
-//
-//
-//int main()
-//{
-//	Student s1("Âº†‰∏â", 1);
-//
-//	//Student s2(s1);
-//	Student s2 = s1;
-//
-//	Student s3("ÊùéÂõõ", 2);
-//
-//	s1 = s3;
-//
-//	cout << &s1 << endl;
-//
-//	return 0;
-//}
-//class Time
-//{
-//	friend class Date; 
-//public:
-//	Time(int hour=1, int minute=1, int second=1)
-//		: _hour(hour)
-//		, _minute(minute)
-//		, _second(second)
-//	{}
-//	void fun()
-//	{
-//		s._year = 1;
-//	}
-//private:
-//	int _hour;
-//	int _minute;
-//	int _second;
-//	Date s;
-//};
-//class Date
-//{
-//public:
-//	Date(int year = 1900, int month = 1, int day = 1)
-//		: _year(year)
-//		, _month(month)
-//		, _day(day)
-//	{}
-//	void SetTimeOfDate(int hour, int minute, int second)
-//	{
-//		_t._hour = hour;
-//		_t._minute = minute;
-//		_t._second = second;
-//	}
-//private:
-//	int _year;
-//	int _month;
-//	int _day;
-//	Time _t;
-//};
-//class Person
-//{
-//public:
-//	Person(const char* name)
-//		: _name(name)
-//	{
-//		cout << "Person()" << endl;
-//	}
-//	Person(const Person& p)
-//		: _name(p._name)
-//	{
-//		cout << "Person(const Person& p)" << endl;
-//	}
-//	Person& operator=(const Person& p)
-//	{
-//		cout << "Person operator=(const Person& p)" << endl;
-//		if (this != &p)
-//			_name = p._name;
-//		return *this;
-//	}
-//	~Person()
-//	{
-//		cout << "~Person()" << endl;
-//	}
-//private:
-//	string _name; // ÂßìÂêç
-//};
-//class Student : public Person
-//	{
-//	public:
-//		Student(const char* name="peter", int num=1)
-//			:Person(name),
-//			_num(num)
-//		{
-//
-//		}
-//		Student(const Student&s)
-//			:Person(s)//ËµãÂÄºËΩ¨Êç¢ÔºåÂàáÁâá
-//             ,_num(s._num)
-//		{}
-//		Student& operator=(const Student& s)
-//		{
-//			if (this != &s)//ÊåáÈíà
-//			{
-//				Person::operator=(s);//Áà∂Á±ªËµãÂÄº
-//				_num = s._num;
-//			}
-//			return *this;
-//		}
-//		~Student()
-//		{
-//			cout << "~Student()" << endl;
-//		
-//		}
-//	protected:
-//		int _num;
-//	};
-//class Person
-//{
-//public:
-//	Person() { ++_count; }
-//protected:
-//	string _name; // ÂßìÂêç
-//public:
-//	static int _count; // ÁªüËÆ°‰∫∫ÁöÑ‰∏™Êï∞„ÄÇ
-//};
-//int Person::_count = 0;
-//class Student : public Person
-//{
-//protected:
-//	int _stuNum; // Â≠¶Âè∑
-//};
-//class Graduate : public Student
-//{
-//protected:
-//	string _seminarCourse; // Á†îÁ©∂ÁßëÁõÆ
-//};
-//void TestPerson()
-//{
-//	Student s1;//++‰∏ÄÊ¨°
-//	Student s2;//++‰∏ÄÊ¨°
-//	Student s3;//++‰∏ÄÊ¨°
-//	Graduate s4;//++‰∏ÄÊ¨°
-//	cout << " ‰∫∫Êï∞ :" << Person::_count << endl;
-//	Student::_count = 0;
-//	cout << " ‰∫∫Êï∞ :" << Person::_count << endl;
-//}
-//int main()
-//{
-//	TestPerson();
-//	return 0;
-//}
-//#include<string>
-//class Student;
-//class Person
-//{
-//public:
-//	friend void Display(const Person& p, const Student& s);
-//protected:
-//	string _name; // ÂßìÂêç
-//};
-//class Student : public Person
-//{
-//protected:
-//	int _stuNum; // Â≠¶Âè∑
-//};
-//void Display(const Person& p, const Student& s)
-//{
-//	cout << p._name << endl;
-//}
-//class Base1 { public: int _b1; };
-//class Base2 { public: int _b2; };
-//					class Derive : public Base1, public Base2 { public: int _d; };
-//					int main() {
-//						Derive d;
-//						Base1* p1 = &d;
-//						Base2* p2 = &d;
-//						Derive* p3 = &d;
-////						return 0;
-////					}
-//class Person
-//{
-//public:
-//	string _name; // ÂßìÂêç
-//};
-//class Student :virtual public Person
-//{
-//protected:
-//	int _num; //Â≠¶Âè∑
-//};
-//class Teacher :virtual public Person
-//{
-//protected:
-//	int _id; // ËÅåÂ∑•ÁºñÂè∑
-//};
-//class Assistant : public Student, public Teacher
-//{
-//protected:
-//	string _majorCourse; // ‰∏ª‰øÆËØæÁ®ã
-//};
-//void Test()
-//{
-//
-//	Assistant a;
-//
-//	a._name = "www";
-//	a.Student::_name = "xxx";
-//	a.Teacher::_name = "yyy";
-//}
-class A
+namespace Key
 {
-public:
-	int _a;
-};
+	template<class K>
+	class BStreeNode
+	{
+	public:
+		BStreeNode(const K& key)
+			:_left(nullptr),
+			_right(nullptr),
+			_key(key)
+		{}
+		BStreeNode<K>* _left;
+		BStreeNode<K>* _right;
+		K _key;
+	};
+	template<class K>
+	class BStree
+	{
+		typedef BStreeNode<K> Node;
+	public:
+		bool Insert(const K& key)
+		{
+			if (_root == nullptr)
+			{
+				_root = new Node(key);
+				return true;
+			}
 
-//class B : public A
-class B : virtual public A
-{
-public:
-	int _b;
-};
+			Node* parent = nullptr;
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else
+				{
+					return false;
+				}
+			}
 
-//class C : public A
-class C : virtual public A
-{
-public:
-	int _c;
-};
+			cur = new Node(key);
+			if (parent->_key < key)
+			{
+				parent->_right = cur;
+			}
+			else
+			{
+				parent->_left = cur;
+			}
 
-class D : public B, public C
+			return true;
+		}
+		void InOrder()//≈≈–Ú
+		{
+			_InOrder(_root);
+		}
+		bool Find(const K& key)//≤È’“
+		{
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					cur = cur->_left;
+				}
+				else
+				{
+					return true;
+				}
+			}
+
+			return true;
+		}
+		bool Erase(const K& key)//…æ≥˝
+		{
+			//»Ù”–“ª∏ˆ◊”Ω⁄µ„£¨…æ≥˝∏∏Ω⁄µ„∫Û£¨»√◊”Ω⁄µ„ÃÓ≥‰
+			//»Ù”–¡©∏ˆ◊”Ω⁄µ„£¨∏∏Ω⁄µ„…æ≥˝∫Û
+			//1.”√◊Û◊” ˜µƒ◊Ó¥ÛΩ⁄µ„ÃÊªª∏∏Ω⁄µ„
+			//2.ªÚ”“◊” ˜µƒ◊Ó–°Ω⁄µ„ÃÊªª∏∏Ω⁄µ„
+			Node* parent = nullptr;
+			Node* cur = _root;
+
+			while (cur)
+			{
+				if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else//’“µΩ¡À
+				{
+					if (cur->_left == nullptr)//»Áπ˚“™…æ≥˝µƒΩ⁄µ„◊ÛŒ™ø’
+					{
+						if (cur == _root)//»Áπ˚“™…æ≥˝µƒ «∏˘Ω⁄µ„(’‚÷÷«Èøˆ∏˘Ω⁄µ„÷ª”–”“◊” ˜£¨“ÚŒ™◊ÛŒ™ø’)
+						{
+							_root = cur->_right;
+						}
+						else
+						{
+							if (cur == parent->_left)//≈–∂œ“™…æ≥˝µƒΩ⁄µ„ «∏∏«◊µƒ◊ÛΩ⁄µ„ªπ «”“Ω⁄µ„
+							{
+								parent->_left = cur->_right;
+							}
+							else
+							{
+								parent->_right = cur->_right;
+							}
+						}
+						delete cur;
+						cur = nullptr;
+					}
+					else if (cur->_right == nullptr)//»Áπ˚“™…æ≥˝µƒΩ⁄µ„”“Œ™ø’
+					{
+						if (cur == _root)
+						{
+							_root = cur->_left;
+						}
+						else
+						{
+							if (cur == parent->_left)//≈–∂œ“™…æ≥˝µƒΩ⁄µ„ «∏∏«◊µƒ◊ÛΩ⁄µ„ªπ «”“Ω⁄µ„
+							{
+								parent->_left = cur->_left;
+							}
+							else
+							{
+								parent->_right = cur->_left;
+							}
+						}
+						delete cur;
+						cur = nullptr;
+					}
+					else//◊Û”“∂ºŒ™ø’£¨“∂◊”Ω⁄µ„,’‚¿Ô≤…”√”√”“ ˜µƒ◊Ó–°Ω⁄µ„Ω¯––…æ≥˝
+					{
+						Node* minParent = cur;
+						Node* min = cur->_right;//cur «“™…æ≥˝µƒΩ⁄µ„
+						while (min->_left)//—∞’“◊Ó–°Ω⁄µ„
+						{
+							minParent = min;
+							min = min->_left;
+						}
+						swap(cur->_key, min->_key);
+						if (minParent->_left == min)
+						{
+							minParent->_left = min->_right;
+						}
+						else
+							minParent->_right = min->_right;
+						delete min;
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+		bool FindR(const K& key)
+		{
+			return _FindR(_root, key);
+		}
+		bool InsertR(const K& key)
+		{
+			return _InsertR(_root, key);
+		}
+		bool EraseR(const K& key)
+		{
+			return _Eraser(_root, key);
+		}
+		~BStree()
+		{
+			Destory(_root);
+		}
+		BStree()
+		{}
+		BStree(const BStree<K>& t)
+		{
+			_root = _Copy(t._root);
+		}
+		BStree<K>& operator=(BStree<K> t)
+		{
+			swap(_root, t._root);
+			return *this;
+		}
+	private:
+		Node* _Copy(Node* root)
+		{
+			if (root == nullptr)
+			{
+				return nullptr;
+			}
+			Node* copyRoot = new Node(root->_key);
+			copyRoot->_left = _Copy(root->_left);
+			copyRoot->_right = _Copy(root->_right);
+			return copyRoot;
+		}
+		void Destory(Node*& root)//≤…”√“˝”√ø…»√root÷√ø’∆◊˜”√
+		{
+			if (root == nullptr)
+				return;
+			Destory(root->_left);
+			Destory(root->_right);
+			delete root;
+			root = nullptr;
+		}
+		bool _Eraser(Node*& root, const K& key)
+		{
+			if (root == nullptr)
+				return false;
+			if (root->_key < key)
+			{
+				return _Eraser(root->_right, key);
+			}
+			else if (root->_key > key)
+			{
+				return _Eraser(root->_left, key);
+			}
+			else
+			{
+				Node* del = root;
+				if (root->_left == nullptr)
+				{
+					root = root->_right;
+				}
+				else if (root->_right == nullptr)
+				{
+					root = root->_left;//”…”⁄ «“˝”√£¨ø…÷±Ω”’‚—˘Ω´∂˛≤Ê ˜¡¨Ω”∆¿¥
+				}
+				else
+				{
+					//’“”“ ˜µƒ◊Ó◊ÛΩ⁄µ„
+					Node* min = root->_right;
+					while (min->_left)
+					{
+						min = min->_left;
+					}
+					swap(root->_key, min->_key);
+					return _Eraser(root->_right, key);
+				}
+				delete del;
+				return true;
+			}
+		}
+		bool _InsertR(Node*& root, const K& key)
+		{
+			if (root == nullptr)//∏˘Œ™ø’£¨÷±Ω”≤Â»Î
+			{
+				root = new Node(key);
+				return true;
+			}
+			if (root->_key < key)
+			{
+				return _InsertR(root->_right, key);
+			}
+			else if (root->_key > key)
+			{
+				return _InsertR(root->_left, key);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		bool _FindR(Node* root, const K& key)
+		{
+			if (root == nullptr)
+				return false;
+			if (root->_key < key)
+			{
+				return _FindR(root->right, key);
+			}
+			else if (root->_key > key)
+			{
+				return _FindR(root->left, key);
+			}
+			else
+			{
+				return true;
+			}
+		}
+		void _InOrder(Node* root)
+		{
+			if (root == nullptr)
+				return;
+			_InOrder(root->_left);
+			cout << root->_key << " ";
+			_InOrder(root->_right);
+		}
+	private:
+		Node* _root = nullptr;
+	};
+	int main()
+	{
+		BStree<int> t;
+		int a[] = { 1,1,2,2,3,6,165,132,4185,123 };
+		for (auto e : a)
+		{
+			t.Insert(e);
+		}
+		BStree<int> copy = t;
+		copy.InOrder();
+		t.InOrder();
+		BStree<int> t1;
+		t1.Insert(2);
+		t1.Insert(1);
+		t1.Insert(3);
+		copy = t1;
+		copy.InOrder();
+		cout << endl;
+		t1.InOrder();
+		cout << endl;
+		return 0;
+	}
+}
+namespace KeyValue
 {
-public:
-	int _d;
-};
-int main()
-{
-	B b;
-	b._a = 1;
-	b._b = 2;
-	cout << sizeof B;
+	template<class K, class V>
+	struct BSTreeNode
+	{
+		BSTreeNode<K, V>* _left;//Key∫ÕValue∞ÛµΩ“ª∆
+		BSTreeNode<K, V>* _right;
+		K _key;
+		V _value;
+
+		BSTreeNode(const K& key, const V& value)
+			:_left(nullptr)
+			, _right(nullptr)
+			, _key(key)
+			, _value(value)
+		{}
+	};
+
+	template<class K, class V>
+	class BSTree
+	{
+		typedef BSTreeNode<K, V> Node;
+	public:
+		bool Insert(const K& key, const V& value)
+		{
+			if (_root == nullptr)
+			{
+				_root = new Node(key, value);
+				return true;
+			}
+
+			Node* parent = nullptr;
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			cur = new Node(key, value);
+			if (parent->_key < key)
+			{
+				parent->_right = cur;
+			}
+			else
+			{
+				parent->_left = cur;
+			}
+
+			return true;
+		}
+
+		Node* Find(const K& key)//≤È’“µƒ ±∫Ú“‘K»•≤È’“£¨∑µªÿµƒ ±∫Ú∑µªÿΩ⁄µ„÷∏’Î£¨“‘±„”⁄–ﬁ∏ƒ
+		{
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					cur = cur->_left;
+				}
+				else
+				{
+					return cur;
+				}
+			}
+
+			return nullptr;
+		}
+
+		bool Erase(const K& key)//”√K…æ≥˝
+		{
+			//...
+
+			return true;
+		}
+
+		void InOrder()
+		{
+			_InOrder(_root);
+			cout << endl;
+		}
+	private:
+
+		void _InOrder(Node* root)
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+
+			_InOrder(root->_left);
+			cout << root->_key << ":" << root->_value << endl;
+			_InOrder(root->_right);
+		}
+	private:
+		Node* _root = nullptr;
+	};
+
+	void TestBSTree1()
+	{
+		BSTree<string, string> dict;
+		dict.Insert("sort", "≈≈–Ú");
+		dict.Insert("left", "◊Û±ﬂ");
+		dict.Insert("right", "”“±ﬂ");
+		dict.Insert("string", "◊÷∑˚¥Æ");
+		dict.Insert("insert", "≤Â»Î");
+		string str;
+		while (cin >> str)
+		{
+			BSTreeNode<string, string>* ret = dict.Find(str);
+			if (ret)
+			{
+				cout << "∂‘”¶µƒ÷–Œƒ:" << ret->_value << endl;
+			}
+			else
+			{
+				cout << "∂‘”¶µƒ÷–Œƒ->Œﬁ¥Àµ•¥ " << endl;
+			}
+		}
+	}
+
+	void TestBSTree2()
+	{
+		string arr[] = { "œ„Ω∂", "∆ªπ˚", "œ„Ω∂", "≤››Æ", "œ„Ω∂", "∆ªπ˚", "∆ªπ˚", "∆ªπ˚" };
+
+		BSTree<string, int> countTree;
+		for (auto& str : arr)
+		{
+			//BSTreeNode<string, int>* ret = countTree.Find(str);
+			auto ret = countTree.Find(str);
+			if (ret)
+			{
+				ret->_value++;
+			}
+			else
+			{
+				countTree.Insert(str, 1);
+			}
+		}
+
+		countTree.InOrder();
+	}
+}
+#include<iostream>
+#include<vector>
+#include<set>
+#include<math.h>
+using namespace std;
+
+void get_yue_shu(int n, vector<int>& a) {
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (n % i == 0) {
+			a.push_back(i);
+			if (n / i != i)
+				a.push_back(n / i);
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	while (cin >> n >> m) {
+		vector<int> res(m + 1, 0);
+		res[n] = 1;
+		for (int i = n; i <= m; i++) {
+			vector<int>a;
+			//Œª÷√iŒﬁ∑®µΩ¥Ô£¨Ã¯π˝
+			if (res[i] == 0)
+				continue;
+			get_yue_shu(i, a);
+			//º«¬º¥”∆µ„µΩiµƒ◊Ó–°≤Ω ˝
+			for (int j = 0; j < a.size(); j++) {
+				//”…µ„i≥ˆ∑¢ƒ‹µΩ¥Ôµƒµ„
+				if ((a[j] + i) <= m && res[a[j] + i] != 0)
+					//∆‰À¸µ„“≤ƒ‹µΩ¥Ô£¨±»Ωœ¥Û–°£¨º«¬º◊Ó–°≤Ω ˝
+					res[a[j] + i] = min(res[a[j] + i], res[i] + 1);
+				else if ((a[j] + i) <= m)
+					//µΩ¥Ôµ„iµƒ◊Ó–°≤Ω ˝º”1
+					res[a[j] + i] = res[i] + 1;
+			}
+		}
+		if (res[m] == 0)
+			cout << -1 << endl;
+		else
+			cout << res[m] - 1 << endl;
+	}
 	return 0;
 }
